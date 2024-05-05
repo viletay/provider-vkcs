79dd151ebfec1986868b416e03e1e5a88b2e485d
Konstantin Kalugin-Balashov <k.balashov@vk.team>
2024-05-05 21:32:02 +0700

    add: vkcs-kubernetes

--- config/provider.go
+++ config/provider.go
@@ -7,6 +7,7 @@ package config
 import (
 	// Note(turkenh): we are importing this to embed provider schema document
 	_ "embed"
+	"github.com/viletay/provider-vkcs/config/kubernetes"
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
 
@@ -43,6 +44,7 @@ func GetProvider() *ujconfig.Provider {
 		disk.Configure,
 		network.Configure,
 		compute.Configure,
+		kubernetes.Configure,
 	} {
 		configure(pc)
 	}
